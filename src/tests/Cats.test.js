c4e363c43f30b9a91b9b5eb0672f8e1c08b92a09
matheusgb <matheusgbr98@gmail.com>
2022-06-24 19:19:47 -0300

    chore: adjusting devDependencies and reinstalling eslint

--- src/tests/Cats.test.js
+++ src/tests/Cats.test.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { render, screen } from '@testing-library/react'
-import App from "../App";
-import Cats from '../components/Cats'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import Cats from '../components/Cats';
 
 describe('Testando componente `Cats`', () => {
-  it('Validando se o componente <Cats /> é renderizado', () => {
-    render(<Cats />);
-  })
-})
+	it('Validando se o componente <Cats /> é renderizado', () => {
+		render(<Cats />);
+	});
+});
