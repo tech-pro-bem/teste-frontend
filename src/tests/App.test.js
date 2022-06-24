e074331dfd2537832704c7dc5071524aaf5f623f
matheusgb <matheusgbr98@gmail.com>
2022-06-24 20:00:22 -0300

    test: validating App contains a header

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from '../pages/App'
 
 describe('Testando componente `App`', () => {
 	it('Validando se a página <App /> é renderizada', () => {
 		render(<App />);
+	});
+
+	it('Validando se a página <App /> contém uma tag `header`', () => {
+		render(<App />);
+		const header = screen.getByRole('banner');
+		expect(header).toBeInTheDocument();
 	});
 });
