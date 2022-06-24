a102349dc238330bef03bd479fe777d44b7a4945
matheusgb <matheusgbr98@gmail.com>
2022-06-24 20:34:50 -0300

    test: validating if Dogs and Cats components contains a button

--- src/tests/Cats.test.js
+++ src/tests/Cats.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Cats from '../components/Cats';
 
 describe('Testando componente `Cats`', () => {
 	it('Validando se o componente <Cats /> existe', () => {
 		render(<Cats />);
 	});
+
+	it('Validando se a página <Cats /> contém uma tag `button`', () => {
+		render(<Cats />);
+		const header = screen.getByRole('button');
+		expect(header).toBeInTheDocument();
+	});
+
 });
--- src/tests/Dogs.test.js
+++ src/tests/Dogs.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Dogs from '../components/Dogs'
 
 describe('Testando componente `Dogs`', () => {
 	it('Validando se o componente <Dogs /> existe', () => {
 		render(<Dogs />);
 	});
+
+  it('Validando se a página <Dogs /> contém uma tag `button`', () => {
+		render(<Dogs />);
+		const header = screen.getByRole('button');
+		expect(header).toBeInTheDocument();
+	});
+
 });
