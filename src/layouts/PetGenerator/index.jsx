0104202058cb613464adbb551a9392a51bb9b363
paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-24 23:08:12 -0300

    feat: add PetGenerator to Dog and Cat

--- src/layouts/PetGenerator/index.jsx
+++ src/layouts/PetGenerator/index.jsx
@@ -1,21 +1,52 @@
+import {
+  useState, useCallback, useMemo,
+} from 'react';
+import axios from 'axios';
+import { Buffer } from 'buffer';
+
 import styles from './style.module.scss';
 import Button from '../../components/Button';
 
 function PetGenerator({ animalType }) {
-  const animalsAvailable = ['Cachorrinho', 'Gatinho'];
-  let animalSelected = '';
+  const animalsAvailable = useMemo(() => ({
+    Cachorrinho: 'https://place.dog/300/400',
+    Gatinho: 'http://placekitten.com/g/300/400',
+  }), []);
 
-  if (!animalsAvailable.find((animal) => animal === animalType)) {
-    animalSelected = 'Cachorrinho';
-  } else { animalSelected = animalType; }
+  const [animalImage, setAnimalImage] = useState(animalType === 'Cachorrinho'
+    ? animalsAvailable.Cachorrinho : animalsAvailable.Gatinho);
+
+  const fetchImage = useCallback(async () => {
+    const url = animalsAvailable[animalType];
+
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          'Cache-Control': 'no-cache',
+          Pragma: 'no-cache',
+          Expires: '0',
+        },
+        withCredentials: false,
+        responseType: 'arraybuffer',
+      });
+      const image = Buffer.from(response.data, 'binary').toString('base64');
+      setAnimalImage(`data:image/jpeg;base64,${image}`);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [animalType, animalsAvailable]);
+
+  const handleClick = async () => {
+    await fetchImage();
+  };
 
   return (
     <div className={styles.petGeneratorContainer}>
-      <img className={styles.image} src="https://place.dog/300/400" alt="asd" />
-      <Button>
+      <img className={styles.image} src={animalImage} alt="asd" />
+      <Button onClick={handleClick}>
         Gerar
         {' '}
-        {animalSelected}
+        {animalType}
       </Button>
     </div>
   );
