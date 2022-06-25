57fc5af8a984ff79bbe9235640fdb0d6d6e63134
matheusgb <matheusgbr98@gmail.com>
2022-06-25 14:11:34 -0300

    style: creating App style

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Animals from '../components/Animals';
 import { AuthContext } from '../providers/auth';
 import { catFetch, dogFetch } from '../services/animalsFetch';
+import * as S from '../styles/App';
 
 function App() {
   const { catAndDog, loading,
@@ -25,16 +26,18 @@ function App() {
   }, []);
 
   return (
-    <>
-      <header>
-        <h1>Doguinhos e Gatinhos</h1>
-      </header>
-      <main>
-        <Animals animal={ catAndDog.cat } />
-        <Animals animal={ catAndDog.dog } />
-        <p>{loading && 'Loading...'}</p>
-      </main>
-    </>
+    <S.AllApp>
+      <S.ContainerApp>
+        <S.HeaderApp>
+          <h1>Doguinhos e Gatinhos</h1>
+        </S.HeaderApp>
+        <S.MainApp>
+          <Animals animal={ catAndDog.cat } />
+          <Animals animal={ catAndDog.dog } />
+          <p>{loading && 'Loading...'}</p>
+        </S.MainApp>
+      </S.ContainerApp>
+    </S.AllApp>
   );
 }
 
---
+++ src/styles/App.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const AllApp = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ContainerApp = styled.div`
+  width: 800px;
+`;
+
+export const HeaderApp = styled.header`
+  display: flex;
+  justify-content: center;
+  margin-bottom:15px;
+`;
+
+export const MainApp = styled.main`
+  display: flex;
+  justify-content: center;
+`;
