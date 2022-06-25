e65da05ba7b118f95554ee0ed072d4dbab0ab8fb
matheusgb <matheusgbr98@gmail.com>
2022-06-25 15:41:07 -0300

    style: adding display none if src is blank

--- src/styles/Animals.js
+++ src/styles/Animals.js
@@ -18,6 +18,7 @@ export const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  display: ${(props) => (props.src === '') && 'none'}
   `;
 
 export const ButtonAnimal = styled.button`
