d04f8b98ef0528218da4f292de174948c501cf8e
e-evaristo <emanoelevaristo@hotmail.com>
2022-06-27 18:04:17 -0300

    #TASK Create Component PetButton

--- src/components/PetsContainer/index.jsx
+++ src/components/PetsContainer/index.jsx
@@ -1,9 +1,11 @@
 import * as C from './styles';
+import PetButton from '../PetButton';
 
 const PetsContainer = () => {
     return ( 
         <C.Container>
-            <h1>Pet Generator</h1>            
+            <h1>Pet Generator</h1>
+            <PetButton label="Gerar Cachorrinho" loading={true} />
         </C.Container>
      );
 }
