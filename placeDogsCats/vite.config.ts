04952a75e0a02c14348ce86cda5c2e3ca5152597
AgnaldoCordeiro <agske13@gmail.com>
2022-06-28 17:18:26 -0300

    arrumando caminho para build

--- placeDogsCats/vite.config.ts
+++ placeDogsCats/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/main.tsx'),
+      entry: path.resolve(__dirname, 'src/pages/main.tsx'),
       name: 'MyLib',
       fileName: (format) => `my-lib.${format}.js`
     },
