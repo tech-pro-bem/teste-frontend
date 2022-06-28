81f90a3c68f50c80e1164fd915c931b5a1af77f4
anajuliarauber <anajuliarauber@hotmail.com>
2022-06-28 14:55:15 -0300

    Adding PetGenerator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { PetGenerator } from "./components/PetGenerator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PetGenerator url="https://place.dog/300/200" title="Gerar cachorrinho"/>
+      <PetGenerator url="http://placekitten.com/200/300" title="Gerar gatinho"/>
+    </>
   );
 }
 
---
+++ src/components/PetGenerator.tsx
@@ -0,0 +1,25 @@
+import {useEffect, useRef} from "react"
+
+interface PetGeneratorProps {
+    url: string,
+    title: string
+}
+
+export function PetGenerator(props: PetGeneratorProps) {
+    const { url, title } = props;
+    const imageRef = useRef<HTMLImageElement>(null)
+
+    function handleClick() {
+        let timestamp = new Date().getTime(); 
+        if(imageRef.current !== null){
+            imageRef.current.src = `${url}?t=${timestamp}`
+        }       
+    }
+
+    return (
+        <>
+            <button onClick={handleClick}>{title}</button>
+            <img src={url} ref={imageRef} alt="imagem de pet" />
+        </>
+    )
+}
