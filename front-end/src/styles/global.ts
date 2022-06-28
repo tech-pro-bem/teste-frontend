5341e33007882fddaad0fc6865d0b2fd50be1dc6
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-06-28 20:40:49 -0300

    feat: add global style settings

---
+++ front-end/src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  html {
+    font-size: 62.5%;
+  }
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+  
+  button {
+    cursor: pointer;
+  }
+`
