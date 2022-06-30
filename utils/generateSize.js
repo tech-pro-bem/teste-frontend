11417353a7c2c38c72c13b291c34c1c9c4d680b9
Isaque.S <isaqueblack129@gmail.com>
2022-06-30 16:05:47 -0300

    feat(generate size): create generate size

---
+++ utils/generateSize.js
@@ -0,0 +1,14 @@
+const SIZES = [300, 400, 500, 600, 700]
+
+export function generateSize() {
+  const height = SIZES[Math.floor(Math.random() * SIZES.length)]
+
+  const numbersReversed = SIZES.reverse()
+  const width =
+    numbersReversed[Math.floor(Math.random() * numbersReversed.length)]
+
+  return {
+    width,
+    height
+  }
+}
