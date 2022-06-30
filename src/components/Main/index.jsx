2656a659a9bb96f533ea1c6c90117e6ef964df6a
Isaque.S <isaqueblack129@gmail.com>
2022-06-30 17:33:29 -0300

    feat(main): create function to generate animal

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,34 @@
+import { useState } from 'react'
+import { Cat, Dog } from 'phosphor-react'
 import { Content } from '../Content'
 import { Container, Heading } from './styled'
-import { Cat, Dog } from 'phosphor-react'
+import { generateSize } from '../../../utils/generateSize'
+
+const INITIAL_STATE = {
+  dog: {
+    height: 300,
+    width: 300
+  },
+
+  cat: {
+    height: 300,
+    width: 300
+  }
+}
 
 export function Main() {
+  const [size, setSize] = useState(INITIAL_STATE)
+
+  function generateAnimal(animal = 'dog') {
+    const { height, width } = generateSize()
+
+    setSize(state => {
+      return animal === 'dog'
+        ? { ...state, dog: { height, width } }
+        : { ...state, cat: { height, width } }
+    })
+  }
+
   return (
     <main>
       <Container>
@@ -12,13 +38,15 @@ export function Main() {
       <Content
         icon={<Cat size={20} />}
         text={'Gerar Gatinho'}
-        url={'http://placekitten.com/200/300'}
+        url={`http://placekitten.com/${size.cat.width}/${size.cat.height}`}
+        onClick={() => generateAnimal('cat')}
       />
 
       <Content
         icon={<Dog size={20} />}
         text={'Gerar Cachorrinho'}
-        url={'http://placekitten.com/200/300'}
+        url={`https://place.dog/${size.dog.width}/${size.dog.height}`}
+        onClick={() => generateAnimal('dog')}
       />
     </main>
   )
