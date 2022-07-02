36fb323a110ea248420c93d2179357883a5876d1
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-02 12:24:25 -0300

    feat: :sparkles: Add api fetching and loader
    Uses count state to perform a re-render and update the animal image url. While fetching, the loader element is displayed.

--- src/components/RandomGenerator.jsx
+++ src/components/RandomGenerator.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Button from './Button';
 import Image from './Image';
+import Loader from './Loader';
+import { getCatImage, getDogImage } from '../utils/api';
 
 const StyledDiv = styled.div`
   display: flex;
@@ -14,6 +16,18 @@ const StyledDiv = styled.div`
 
 function RandomGenerator({ animal }) {
   const [cat, setCat] = useState(parseInt(Math.random() * (16 - 0) + 0, 10));
+  const [animalImage, setAnimalImage] = useState(undefined);
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const fetchImageURL = async () => {
+    if (animal === 'gato') {
+      setAnimalImage(await getCatImage(cat));
+    } else {
+      setAnimalImage(await getDogImage());
+    }
+    await setLoading(false);
+  };
 
   const generateNewCat = () => {
     const max = 16;
@@ -24,23 +38,30 @@ function RandomGenerator({ animal }) {
     );
   };
 
-    setSize({
-      width: parseInt(Math.random() * (max - min) + min, 10),
-      height: parseInt(Math.random() * (max - min) + min, 10),
-    });
+  const incrementCount = () => {
+    setCount(count + 1);
   };
+
+  useEffect(() => {
+    setLoading(true);
+    if (animal === 'gato') {
+      generateNewCat();
+    }
+    fetchImageURL();
+  }, [count]);
 
   return (
     <StyledDiv>
-      <Button animal={animal} onClick={generateNewSize} />
-      <Image api={api} size={size} animal={animal} />
+      <Button animal={animal} onClick={incrementCount} />
+      { loading || !animalImage
+        ? <Loader />
+        : <Image api={animalImage} animal={animal} />}
     </StyledDiv>
   );
 }
 
 RandomGenerator.propTypes = {
   animal: PropTypes.string.isRequired,
-  api: PropTypes.string.isRequired,
 };
 
 export default RandomGenerator;
