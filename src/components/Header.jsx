6526abde2e71ad5b04098992bf47b6af2e49f1be
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-02 02:54:23 -0300

    style: :lipstick: Add style to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  color: rgb(49, 102, 148);
+  font-size: 2em;
+  text-align: center;
+  margin-top: 2em;
+`;
 
 function Header() {
   return (
-    <header>
+    <StyledHeader>
       Gerador de animais
-    </header>
+    </StyledHeader>
   );
 }
 
