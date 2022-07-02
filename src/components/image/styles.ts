7407afbcafc50289a80f045eb912979ab606ab20
YuriMendess <contato.yurimendess@gmail.com>
2022-07-02 20:45:15 -0300

    adição da dependência react-icons e criação do componente image

---
+++ src/components/image/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Image = styled.div`
+  width: 100%;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
