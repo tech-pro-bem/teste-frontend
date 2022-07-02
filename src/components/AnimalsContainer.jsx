8a8437ad42fcfdb905db400507b43e6a59ef7d49
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-02 02:55:55 -0300

    style: :lipstick: Add style to animals generator's component

--- src/components/AnimalsContainer.jsx
+++ src/components/AnimalsContainer.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import RandomGenerator from './RandomGenerator';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: center;
+`;
 
 function AnimalsContainer() {
   return (
-    <div>
+    <StyledContainer>
       <RandomGenerator animal="gato" api="http://placekitten.com" />
       <RandomGenerator animal="cachorro" api="https://place.dog" />
-    </div>
+    </StyledContainer>
   );
 }
 
