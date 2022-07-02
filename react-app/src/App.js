5009d1c6e62b398f4dca00f9b4de88e03ecf074a
LucasG-Nunes <lucasgnam12@gmail.com>
2022-07-02 13:51:35 -0300

    :construction: Construção do app.js reutilizando o componente.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,7 +4,8 @@ import ReusableComponent from './Components/ReusableComponent';
 function App() {
   return (
     <div className="App">
-      <ReusableComponent url ={'http://placekitten.com'} animal={"gatinho"}/>
+      <ReusableComponent url={'http://placekitten.com'} animal={'gatinho'}/>
+      <ReusableComponent url={'https://place.dog'} animal={'cachorro'}/>
     </div>
   );
 }
