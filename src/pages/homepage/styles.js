5754d5c71a2e914edb367615111f7252be16e6a3
PaulaCBS <paulasalvado2019@gmail.com>
2022-07-02 16:54:02 -0300

    feature: add background images on homepage

--- src/pages/homepage/styles.js
+++ src/pages/homepage/styles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import catpic from '../../assets/images/cat.png'
+import dogpic from '../../assets/images/dog.png'
 
 export const PageContent = styled.main`
     display: flex;
@@ -9,4 +11,7 @@ export const PageContent = styled.main`
     width: 100%;
     min-height: calc(100vh - 8.4rem);
     padding: 2rem 5%;
+    background: 
+        url(${catpic}) no-repeat left bottom/30vw,
+        url(${dogpic}) no-repeat right bottom/25vw;
 `
