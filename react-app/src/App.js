d1cc51cead08323696e921fb6e90070fac7fd9c7
LucasG-Nunes <lucasgnam12@gmail.com>
2022-07-02 13:02:23 -0300

    :truck: atualizando o App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ReusableComponent from './Components/ReusableComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ReusableComponent url ={'http://placekitten.com'} animal={"gatinho"}/>
     </div>
   );
 }
