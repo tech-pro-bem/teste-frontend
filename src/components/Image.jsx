3ffab6c06c69d31a87b7ff736141a86efb233b5b
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-02 12:23:09 -0300

    refactor: :fire: Remove size props

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -6,16 +6,15 @@ const StyledImage = styled.img`
   border-radius: 2rem;
 `;
 
-function Image({ api, size, animal }) {
+function Image({ api, animal }) {
   return (
-    <StyledImage src={`${api}/${size.width}/${size.height}`} alt={`Foto gerada randomicamente de um ${animal}`} />
+    <StyledImage src={api} alt={`Foto gerada randomicamente de um ${animal}`} />
   );
 }
 
 Image.propTypes = {
   animal: PropTypes.string.isRequired,
   api: PropTypes.string.isRequired,
-  size: PropTypes.objectOf(number).isRequired,
 };
 
 export default Image;
