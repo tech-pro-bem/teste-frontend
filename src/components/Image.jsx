406a75c79e6dd02de83f536a4fd6ec5cf7bf82a4
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-02 12:22:53 -0300

    refactor: :fire: Remove number from prop-types

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes, { number } from 'prop-types';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const StyledImage = styled.img`
