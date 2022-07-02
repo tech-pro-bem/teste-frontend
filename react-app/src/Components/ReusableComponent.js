8c21dd90c52f301c471563e843ec59c5b561f4af
LucasG-Nunes <lucasgnam12@gmail.com>
2022-07-02 14:42:20 -0300

    :rocket: aplicando regras para view.

--- react-app/src/Components/ReusableComponent.js
+++ react-app/src/Components/ReusableComponent.js
@@ -1,44 +1,66 @@
 import { useState } from "react";
 import axios from "axios";
-import Styles from './ReusableComponent.module.css'
-const ReusableComponent = ({url, animal }) => {
-  const [width, setWidth] = useState(200);
-  const [height, setHeight] = useState(200);
+import Styles from "./ReusableComponent.module.css";
+
+const ReusableComponent = ({ url, animal }) => {
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState();
 
   const [image, setImage] = useState("");
-
+  const [imageDog, setImageDog] = useState("");
 
   async function getCat() {
     const response = axios
       .get(`${url}/${width}/${height}`, { responseType: "blob" })
       .then((response) => {
         setImage(URL.createObjectURL(response.data));
+        console.log("houve exito na sua conexão", response.status);
       })
       .catch((err) => {
         console.log("error", err);
       });
   }
 
+  async function getDog() {
+    const response = await axios
+      .get(`${url}`, {
+        headers: {
+          "X-RapidAPI-Key":
+            "bcd410e343msh4d5772c71f54d4dp1b8663jsna8fba6394842",
+          "X-RapidAPI-Host": "placedog.p.rapidapi.com",
+        },
+        responseType: "blob",
+      })
+      .then((response) => {
+        setImageDog(URL.createObjectURL(response.data));
+        console.log("houve exito na sua conexão", response.status);
+      })
+      .catch((err) => {
+        console.log("houve um erro", err);
+      });
+  }
 
   return (
-<>
-    <div className={Styles['container']}>
-
-    <h1 className={Styles['h1']}>Pet Generator</h1>
-      <button
-        className={Styles['button']}
-        onClick={() => {
-          setWidth(Math.floor(Math.random() * 500) + 200);
-          setHeight( Math.floor(Math.random() * 500)+ 200);
-          getCat()
-        }}
-      >
-        Gerar {animal}
-      </button>
-      <img className={Styles['image']} src={image}></img>
-    
-
-    </div>
+    <>
+      <div className={Styles["container"]}>
+        <h1 className={Styles["h1"]}>{animal} Generator</h1>
+        <button
+          className={Styles["button"]}
+          onClick={() => {
+            setWidth(Math.floor(Math.random() * 500) + 200);
+            setHeight(Math.floor(Math.random() * 500) + 200);
+            {
+              animal == "Gatinho" ? getCat()  : getDog();
+            }
+          }}
+        >
+          Gerar {animal}
+        </button>
+        <img
+          className={Styles["image"]}
+          src={animal == "Gatinho" ? image : imageDog}
+        ></img>
+      </div>
     </>
   );
 };
