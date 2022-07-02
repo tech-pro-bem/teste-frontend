ed7998e8368e1eef8022e2c4dbbb84b14f9fddd5
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-02 02:54:02 -0300

    style: :lipstick: Add style to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// Ref.: https://cssbuttons.app/buttons/3
+const StyledButton = styled.button`
+  display: inline-block;
+  background-color: rgb(49, 102, 148);
+  padding: 1em 1.7em;
+  margin: 0 0.1em 0.1em 0;
+  border: 0.3em solid rgb(255, 255, 255);
+  border-radius: 2em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: "Roboto", sans-serif;
+  font-weight: 300;
+  color: #ffffff;
+  text-shadow: 0 0.04em 0.04em rgba(255, 255, 255, 0.253);
+  text-align: center;
+  transition: all 0.2s;
+  &:hover {
+    color: black;
+    background-color: rgb(100, 153, 200);
+  }
+`;
 
 function Button({ animal, onClick }) {
   return (
-    <button type="button" onClick={onClick}>
+    <StyledButton type="button" onClick={onClick}>
       {`Gerar um novo ${animal}`}
-    </button>
+    </StyledButton>
   );
 }
 
